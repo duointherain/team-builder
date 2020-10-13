73d4cb8ce6b1a9ecff7fbab373b56034a6d72b86
duointherain <duointherain@gmail.com>
2020-10-13 13:24:35 -0700

    teamForm

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Teammember from "./Teammember";
-import FriendForm from "./FriendForm";
+import TeamForm from "./TeamForm";
 import axios from "../axios";
 
 function App() {
